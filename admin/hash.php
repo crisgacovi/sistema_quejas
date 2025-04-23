feaa5a69a01c19745db2284c19f0c0b89b3355fd
crisgacovi <49741384+crisgacovi@users.noreply.github.com>
2025-04-23 14:13:49 -0500

    Delete admin directory

--- admin/hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo password_hash("admin123", PASSWORD_DEFAULT)."\n"."<br>";//administrador, Cristian Coronado
-echo password_hash("auditor123", PASSWORD_DEFAULT)."\n"."<br>";//auditor, Eduard Escamilla
-
-
-?>
-
